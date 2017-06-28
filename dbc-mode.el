40ce52101970b23e7ebe195b975d988e73b73c89
Alex Murray <murray.alex@gmail.com>
2017-06-28 16:43:40 +0930

    Add copyright and package header

--- dbc-mode.el
+++ dbc-mode.el
@@ -1,6 +1,36 @@
-;;; dbc-mode.el --- major mode for CAN dbc files
+;;; dbc-mode.el --- major mode for CAN dbc files         -*- lexical-binding: t; -*-
+
+;; Copyright (c) 2017 Alex Murray
+
+;; Author: Alex Murray <murray.alex@gmail.com>
+;; Maintainer: Alex Murray <murray.alex@gmail.com>
+;; URL: https://github.com/alexmurray/dbc-mode
+;; Version: 0.1
+;; Package-Requires: ((emacs "24.4"))
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
+
+;; This packages provides syntax highlighting and imenu support for CAN dbc
+;; files
+
+;;;; Setup
+
+;; (require 'dbc-mode)
 
 ;;; Code:
 
